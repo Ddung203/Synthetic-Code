fc155f47a9ac8b6dc60b6a91a10057e8d886661e
Ddung203 <duongvandung2k3@gmail.com>
2022-05-07 18:18:36 +0700

    So Chinh Phuong

---
+++ soChinhPhuong.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int cp(long long n)
+{
+  int can = sqrt(n) + 0.5;
+  if (1ll * can * can == n)
+    return 1;
+  return 0;
+}
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b;
+    cin >> a >> b;
+    int c1 = sqrt(a), c2 = sqrt(b);
+    if (c1 * c1 != a)
+      ++c1;
+    for (int i = c1; i <= c2; i++)
+      cout << i * i << " ";
+    cout << endl;
+  }
+  return 0;
+}
